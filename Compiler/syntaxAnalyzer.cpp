de72dbadf68f25f702a123f8705e2f32c118960d
IDragonThunderI <andrey.voltr@yandex.ru>
2024-03-19 03:49:44 +0400

    Реструктурировал репозиторий
    Вынес проект Compiler (do-while) в отдельную ветвь.

--- Compiler/syntaxAnalyzer.cpp
+++ Compiler/syntaxAnalyzer.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "syntaxAnalyzer.hpp"
 using namespace std;
 
 int buf_i, num_tokens = 0;
@@ -10,14 +11,14 @@ vector<string> buf_lexem;
 vector<string> derevo_obrat;
 
 char rule_matrix[21][21] = {
-    //       ;    ?    (    )    :    {    }    a    c   :=   or   and   <    >    =   not   -    +    *    /    #
+    //       ;  while  (    )   do    {    }    a    c   :=   or   and   <    >    =   not   -    +    *    /    #
     /* ; */ '>', ' ', ' ', ' ', '>', '<', '>', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '>',
-    /* ? */ '>', ' ', '<', ' ', '=', '<', ' ', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+  /*while*/ ' ', ' ', '<', ' ', '<', ' ', ' ', ' ', ' ', ' ', '<', '<', ' ', ' ', ' ', '<', ' ', ' ', ' ', ' ', '>',
     /* ( */ ' ', ' ', '<', '=', ' ', ' ', ' ', '<', '<', ' ', '<', '<', '<', '<', '<', '<', '<', '<', '<', '<', ' ',
-    /* ) */ '>', '>', ' ', '>', '>', '<', '>', '<', ' ', '>', '>', '>', '>', '>', '>', ' ', '>', '>', '>', '>', ' ',
-    /* : */ '>', ' ', ' ', ' ', '>', '<', '>', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '>',
+    /* ) */ '>', '<', ' ', '>', ' ', '<', '>', '<', ' ', '>', '>', '>', '>', '>', '>', ' ', '>', '>', '>', '>', '>',
+    /*do */ '<', '=', ' ', ' ', ' ', '<', '>', '<', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '>',
     /* { */ '<', ' ', ' ', '>', '<', ' ', '=', '<', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-    /* } */ '>', ' ', ' ', ' ', '>', ' ', '>', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '>',
+    /* } */ '>', '>', ' ', ' ', ' ', ' ', '>', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '>',
     /* a */ '>', ' ', ' ', '>', '>', ' ', '>', ' ', ' ', '=', '>', '>', '>', '>', '>', ' ', '>', '>', '>', '>', ' ',
     /* c */ '>', ' ', ' ', '>', '>', ' ', '>', ' ', ' ', ' ', '>', '>', '>', '>', '>', ' ', '>', '>', '>', '>', ' ',
     /*:= */ '>', ' ', '<', ' ', '>', '>', '<', '<', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '<', '<', '<', '<', ' ',
@@ -25,138 +26,134 @@ char rule_matrix[21][21] = {
     /*and*/ '>', ' ', '<', '>', '>', ' ', ' ', '<', '<', ' ', '>', '<', '<', '<', '<', '<', '<', '<', '<', '<', ' ',
     /* < */ ' ', ' ', '<', '>', ' ', ' ', ' ', '<', '<', ' ', '>', '>', ' ', ' ', ' ', ' ', '<', '<', '<', '<', ' ',
     /* > */ ' ', ' ', '<', '>', ' ', ' ', ' ', '<', '<', ' ', '>', '>', ' ', ' ', ' ', ' ', '<', '<', '<', '<', ' ',
-    /* = */ ' ', ' ', '<', '>', ' ', ' ', ' ', '<', '<', ' ', '>', '>', ' ', ' ', ' ', ' ', '<', '<', '<', '<', ' ',
-    /*not*/ '<', ' ', '<', '>', ' ', ' ', ' ', '<', ' ', ' ', '>', '>', '<', '<', '<', ' ', '>', '>', '>', '>', ' ',
-    /* - */ '>', ' ', '<', '>', '>', ' ', '>', '<', '<', ' ', '>', '>', '>', '>', '>', ' ', '>', '>', '<', '<', ' ',
-    /* + */ '>', ' ', '<', '>', '>', ' ', '>', '<', '<', ' ', '>', '>', '>', '>', '>', ' ', '>', '>', '<', '<', ' ',
-    /* * */ '>', ' ', '<', '>', '>', ' ', '>', '<', '<', ' ', '>', '>', '>', '>', '>', ' ', '>', '>', '>', '>', ' ',
-    /* / */ '>', ' ', '<', '>', '>', ' ', '>', '<', '<', ' ', '>', '>', '>', '>', '>', ' ', '>', '>', '>', '>', ' ',
-    /* $ */ '<', '<', '<', ' ', ' ', '<', ' ', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '<', '<', '<', '<', ' '
+    /* = */ ' ', ' ', '<', '>', '>', ' ', ' ', '<', '<', ' ', '>', '>', ' ', ' ', ' ', ' ', '<', '<', '<', '<', ' ',
+    /*not*/ '<', ' ', '<', '>', '>', ' ', ' ', '<', ' ', ' ', '>', '>', '<', '<', '<', ' ', '>', '>', '>', '>', ' ',
+    /* - */ '>', ' ', '<', '>', ' ', ' ', '>', '<', '<', ' ', '>', '>', '>', '>', '>', ' ', '>', '>', '<', '<', ' ',
+    /* + */ '>', ' ', '<', '>', ' ', ' ', '>', '<', '<', ' ', '>', '>', '>', '>', '>', ' ', '>', '>', '<', '<', ' ',
+    /* * */ '>', ' ', '<', '>', ' ', ' ', '>', '<', '<', ' ', '>', '>', '>', '>', '>', ' ', '>', '>', '>', '>', ' ',
+    /* / */ '>', ' ', '<', '>', ' ', ' ', '>', '<', '<', ' ', '>', '>', '>', '>', '>', ' ', '>', '>', '>', '>', ' ',
+    /* $ */ '<', '<', '<', ' ', '<', '<', ' ', '<', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '<', '<', '<', '<', ' '
 };
 
 char Get_Relations(string left, string right) {
     int l = -1, r = -1;
+    
+    if (left == ";")          l = 0;
+    else if (left == "while") l = 1;
+    else if (left == "(")     l = 2;
+    else if (left == ")")     l = 3;
+    else if (left == "do")    l = 4;
+    else if (left == "{")     l = 5;
+    else if (left == "}")     l = 6;
+    else if (left == "a")     l = 7;
+    else if (left == "c")     l = 8;
+    else if (left == ":=")    l = 9;
+    else if (left == "or")    l = 10;
+    else if (left == "and")   l = 11;
+    else if (left == "<")     l = 12;
+    else if (left == ">")     l = 13;
+    else if (left == "=")     l = 14;
+    else if (left == "not")   l = 15;
+    else if (left == "-")     l = 16;
+    else if (left == "+")     l = 17;
+    else if (left == "*")     l = 18;
+    else if (left == "/")     l = 19;
+    else if (left == "$")     l = 20;
 
-    if (left == ";")        l = 0;
-    else if (left == "?")   l = 1;
-    else if (left == "(")   l = 2;
-    else if (left == ")")   l = 3;
-    else if (left == ":")   l = 4;
-    else if (left == "{")   l = 5;
-    else if (left == "}")   l = 6;
-    else if (left == "a")   l = 7;
-    else if (left == "c")   l = 8;
-    else if (left == ":=")  l = 9;
-    else if (left == "or")  l = 10;
-    else if (left == "and") l = 11;
-    else if (left == "<")   l = 12;
-    else if (left == ">")   l = 13;
-    else if (left == "=")   l = 14;
-    else if (left == "not") l = 15;
-    else if (left == "-")   l = 16;
-    else if (left == "+")   l = 17;
-    else if (left == "*")   l = 18;
-    else if (left == "/")   l = 19;
-    else if (left == "$")   l = 20;
-
-    if (right == ";")        r = 0;
-    else if (right == "?")   r = 1;
-    else if (right == "(")   r = 2;
-    else if (right == ")")   r = 3;
-    else if (right == ":")   r = 4;
-    else if (right == "{")   r = 5;
-    else if (right == "}")   r = 6;
-    else if (right == "a")   r = 7;
-    else if (right == "c")   r = 8;
-    else if (right == ":=")  r = 9;
-    else if (right == "or")  r = 10;
-    else if (right == "and") r = 11;
-    else if (right == "<")   r = 12;
-    else if (right == ">")   r = 13;
-    else if (right == "=")   r = 14;
-    else if (right == "not") r = 15;
-    else if (right == "-")   r = 16;
-    else if (right == "+")   r = 17;
-    else if (right == "*")   r = 18;
-    else if (right == "/")   r = 19;
-    else if (right == "#")   r = 20;
+    if (right == ";")          r = 0;
+    else if (right == "while") r = 1;
+    else if (right == "(")     r = 2;
+    else if (right == ")")     r = 3;
+    else if (right == "do")    r = 4;
+    else if (right == "{")     r = 5;
+    else if (right == "}")     r = 6;
+    else if (right == "a")     r = 7;
+    else if (right == "c")     r = 8;
+    else if (right == ":=")    r = 9;
+    else if (right == "or")    r = 10;
+    else if (right == "and")   r = 11;
+    else if (right == "<")     r = 12;
+    else if (right == ">")     r = 13;
+    else if (right == "=")     r = 14;
+    else if (right == "not")   r = 15;
+    else if (right == "-")     r = 16;
+    else if (right == "+")     r = 17;
+    else if (right == "*")     r = 18;
+    else if (right == "/")     r = 19;
+    else if (right == "#")     r = 20;
 
     if (l == -1 || r == -1) {
         cout << "Left is " << left << " right is " << right << "\nERROR with l=" << l << " and r=" << r << endl;
         return '~';
     }
-
+    
     return rule_matrix[l][r];
 }
-
+                                       
 int Get_Rule(string search_rule) {
-    if (search_rule == "E;E")           return 2;
-    else if (search_rule == "E;")       return 3;
-    else if (search_rule == "B?E")      return 4;
-    else if (search_rule == "B?E:E")    return 5;
-    else if (search_rule == "{E}")      return 6;
-    else if (search_rule == "a:=E")     return 7;
-    else if (search_rule == "BandB")    return 8;
-    else if (search_rule == "BorB")     return 9;
-    else if (search_rule == "notB")     return 10;
-    else if (search_rule == "(B)")      return 11;
-    else if (search_rule == "E<E")      return 12;
-    else if (search_rule == "E=E")      return 13;
-    else if (search_rule == "E>E")      return 14;
-    else if (search_rule == "E-E")      return 15;
-    else if (search_rule == "E+E")      return 16;
-    else if (search_rule == "E*E")      return 17;
-    else if (search_rule == "E/E")      return 18;
-    else if (search_rule == "(E)")      return 19;
-    else if (search_rule == "a")        return 20;
-    else if (search_rule == "c")        return 21;
-    else                                return -1;
+    if (search_rule == "E;E")               return 2;
+    else if (search_rule == "E;")           return 3;
+    else if (search_rule == "doEwhileB")    return 4;
+    else if (search_rule == "{E}")          return 5;
+    else if (search_rule == "a:=E")         return 6;
+    else if (search_rule == "BandB")        return 7;
+    else if (search_rule == "BorB")         return 8;
+    else if (search_rule == "notB")         return 9;
+    else if (search_rule == "(B)")          return 10;
+    else if (search_rule == "E<E")          return 11;
+    else if (search_rule == "E=E")          return 12;
+    else if (search_rule == "E>E")          return 13;
+    else if (search_rule == "E-E")          return 14;
+    else if (search_rule == "E+E")          return 15;
+    else if (search_rule == "E*E")          return 16;
+    else if (search_rule == "E/E")          return 17;
+    else if (search_rule == "(E)")          return 18;
+    else if (search_rule == "a")            return 19;
+    else if (search_rule == "c")            return 20;
+    else                                    return -1;
 }
 
 void derevo(int g) {
     vector<string> bufer;
     bufer.clear();
     int buf_now = 0;
-
     if (rules[buf_i] == 2) {
         bufer.push_back("E"); bufer.push_back(";"); bufer.push_back("E");
     } else if (rules[buf_i] == 3) {
         bufer.push_back("E"); bufer.push_back(";");
     } else if (rules[buf_i] == 4) {
-        bufer.push_back("B"); bufer.push_back("?"); bufer.push_back("E");
+        bufer.push_back("do"); bufer.push_back("E"); bufer.push_back("while"); bufer.push_back("B");
     } else if (rules[buf_i] == 5) {
-        bufer.push_back("B"); bufer.push_back("?"); bufer.push_back("E"); bufer.push_back(":"); bufer.push_back("E");
-    } else if (rules[buf_i] == 6) {
         bufer.push_back("{"); bufer.push_back("E"); bufer.push_back("}");
-    } else if (rules[buf_i] == 7) {
+    } else if (rules[buf_i] == 6) {
         bufer.push_back("a"); bufer.push_back(":="); bufer.push_back("E");
-    } else if (rules[buf_i] == 8) {
+    } else if (rules[buf_i] == 7) {
         bufer.push_back("B"); bufer.push_back("and"); bufer.push_back("B");
-    } else if (rules[buf_i] == 9) {
+    } else if (rules[buf_i] == 8) {
         bufer.push_back("B"); bufer.push_back("or"); bufer.push_back("B");
-    } else if (rules[buf_i] == 10) {
+    } else if (rules[buf_i] == 9) {
         bufer.push_back("not"); bufer.push_back("B");
-    } else if (rules[buf_i] == 11) {
+    } else if (rules[buf_i] == 10) {
         bufer.push_back("("); bufer.push_back("B"); bufer.push_back(")");
-    } else if (rules[buf_i] == 12) {
+    } else if (rules[buf_i] == 11) {
         bufer.push_back("E"); bufer.push_back("<"); bufer.push_back("E");
-    } else if (rules[buf_i] == 13) {
+    } else if (rules[buf_i] == 12) {
         bufer.push_back("E"); bufer.push_back("="); bufer.push_back("E");
-    } else if (rules[buf_i] == 14) {
+    } else if (rules[buf_i] == 13) {
         bufer.push_back("E"); bufer.push_back(">"); bufer.push_back("E");
-    } else if (rules[buf_i] == 15) {
+    } else if (rules[buf_i] == 14) {
         bufer.push_back("E"); bufer.push_back("-"); bufer.push_back("E");
-    } else if (rules[buf_i] == 16) {
+    } else if (rules[buf_i] == 15) {
         bufer.push_back("E"); bufer.push_back("+"); bufer.push_back("E");
-    } else if (rules[buf_i] == 17) {
+    } else if (rules[buf_i] == 16) {
         bufer.push_back("E"); bufer.push_back("*"); bufer.push_back("E");
-    } else if (rules[buf_i] == 18) {
+    } else if (rules[buf_i] == 17) {
         bufer.push_back("E"); bufer.push_back("/"); bufer.push_back("E");
-    } else if (rules[buf_i] == 19) {
+    } else if (rules[buf_i] == 18) {
         bufer.push_back("("); bufer.push_back("E"); bufer.push_back(")");
-    } else if (rules[buf_i] == 20) {
+    } else if (rules[buf_i] == 19) {
         bufer.push_back("a");
-    } else if (rules[buf_i] == 21) {
+    } else if (rules[buf_i] == 20) {
         bufer.push_back("c");
     }
 
@@ -286,7 +283,7 @@ void s_analyz(ofstream& file_out_SR) {
                 file_out_SR << " \"" << gamma << "\" по правилу " << rules[ruleCount] << " | ";
                 if (rules[ruleCount] != -1) {
                     ruleCount++;
-                    buf_lexem[j] = (rules[ruleCount - 1] >= 8 && rules[ruleCount - 1] <= 14) ? "B" : "E";
+                    buf_lexem[j] = (rules[ruleCount - 1] >= 7 && rules[ruleCount - 1] <= 13) ? "B" : "E";
 
                     for (int i = 0; i < ruleCount; i++) {
                         file_out_SR << rules[i] << " ";
@@ -324,8 +321,8 @@ void s_analyz(ofstream& file_out_SR) {
     }
 }
 
-void Syntax_Analyz() {
-    setlocale(LC_ALL, "");
+void syntaxAnalysis() {
+setlocale(LC_ALL, "");
     ofstream file_out_SR;
     file_out_SR.open("out_SR.txt");
 
@@ -351,9 +348,4 @@ void Syntax_Analyz() {
 
     file_out_SR.close();
     file_derevo.close();
-}
-
-int main() {
-    Syntax_Analyz();
-    return 0;
 }
